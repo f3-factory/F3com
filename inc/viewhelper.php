e2bd4a6790cae8d8187cd3858627c366a6e47b98
Sascha Ohms <sasch9r@gmail.com>
2020-01-09 09:23:00 +0100

    fix: use f3 ^3.7 + fix ternary compatibility

--- inc/viewhelper.php
+++ inc/viewhelper.php
@@ -18,7 +18,7 @@ class ViewHelper
 	static function getBaseUrl()
 	{
 		$f3 = \Base::instance();
-		$s = empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on") ? "s" : "";
+		$s = (empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on")) ? "s" : "";
 		$protocol = substr(strtolower($_SERVER["SERVER_PROTOCOL"]), 0,
 			strpos(strtolower($_SERVER["SERVER_PROTOCOL"]), "/")).$s;
 		$port = ($_SERVER["SERVER_PORT"] == "80") ? "" : (":".$_SERVER["SERVER_PORT"]);
